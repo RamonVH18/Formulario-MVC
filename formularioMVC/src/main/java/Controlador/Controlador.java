5369d95d5e24347deca21c41bcf4f9dfb82ca945
TRMrDucky <vv094673@gmail.com>
2025-09-21 15:26:20 -0700

    /

--- formularioMVC/src/main/java/Controlador/Controlador.java
+++ formularioMVC/src/main/java/Controlador/Controlador.java
@@ -4,10 +4,33 @@
  */
 package Controlador;
 
+import Entidades.Usuario;
+import java.util.List;
+import java.util.LinkedList;
+
 /**
  *
  * @author 52644
  */
 public class Controlador {
+    
+    private List<Usuario> usuarios;
+    private static Controlador controlador;
+    
+    private Controlador() {
+        usuarios = new LinkedList<>();
+    }
+    
+    public static Controlador getControlador(){
+        if (controlador == null)
+                controlador = new Controlador();
+        return controlador;
+    }
+    
+    public void registrarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+    
+    
     
 }
--- formularioMVC/src/main/java/Entidades/Usuario.java
+++ formularioMVC/src/main/java/Entidades/Usuario.java
@@ -9,5 +9,47 @@ package Entidades;
  * @author 52644
  */
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
+    
     
 }
